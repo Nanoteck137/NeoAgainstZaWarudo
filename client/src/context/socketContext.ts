7063b20d1590bde82cca3a82fc0cf268204bcc9f
Theo Sandell <theo01sandell@gmail.com>
2022-06-13 03:55:17 +0200

    Update socketContext.ts

--- client/src/context/socketContext.ts
+++ client/src/context/socketContext.ts
@@ -2,6 +2,5 @@ import { createContext } from "react";
 import { io, Socket } from "socket.io-client";
 
 export const socket = io(process.env.REACT_APP_SERVER_URL ?? "");
-console.log(socket);
 
 export const SocketContext = createContext(socket);
