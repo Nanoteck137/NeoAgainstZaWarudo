afaeee6f89b73b6f76291ae879b9391b38215ae7
Patrik M. Rosenström <patrik.millvik@gmail.com>
2022-06-15 00:43:08 +0200

    Working on new interfaces for the game

---
+++ client/src/types/game.ts
@@ -0,0 +1,17 @@
+import { ServerPlayer } from "./server";
+
+interface GamePlayer {
+  /*
+  player: ServerPlayer,
+  hand: any[], // WhiteCard[]
+  */
+}
+
+export interface Game {
+  /*
+  players: ServerPlayer[],
+  currentPlayer: GamePlayer,
+  blackCard: any, // BlackCard
+  currentJudgeId: string,
+  */
+}
