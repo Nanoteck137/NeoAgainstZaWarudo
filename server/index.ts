d5f6852284395d76a78e222100d2bced356bfe1c
Patrik M. Rosenström <patrik.millvik@gmail.com>
2022-06-15 03:03:31 +0200

    Added the ability to change game settings

--- server/index.ts
+++ server/index.ts
@@ -1,17 +1,28 @@
-import express, { Request, Response } from "express"
-import dotenv from "dotenv"
-import http from "http"
-import { Server, Socket } from "socket.io"
-import cors from "cors"
-import { getPlayerBySocket, Player, players, registerNewPlayer } from "./player"
-import { deleteRoomById, getRoomById, registerNewRoom, Room, rooms } from "./room"
-import { games } from "./game"
-import { readCardList } from "./card"
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import http from "http";
+import { Server, Socket } from "socket.io";
+import cors from "cors";
+import {
+  getPlayerBySocket,
+  Player,
+  players,
+  registerNewPlayer,
+} from "./player";
+import {
+  deleteRoomById,
+  getRoomById,
+  registerNewRoom,
+  Room,
+  rooms,
+} from "./room";
+import { games, GameSettings } from "./game";
+import { getCardPacks, readCardList } from "./card";
 
 dotenv.config({ path: "../.env" });
 
 /// TODO(patrik):
-///   - When a player left/disconnects we need to cleanup the game if 
+///   - When a player left/disconnects we need to cleanup the game if
 ///     one has started
 ///   - Add player count to the room structure
 ///   - Refactor the sending of server infomation to the client
@@ -19,15 +30,14 @@ dotenv.config({ path: "../.env" });
 ///   - When we delete a room we also need to check and delete the game
 ///   - Change rooms:leave to room:leave?
 
-
 const app = express();
 const port = process.env.SERVER_PORT || 3000;
 
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: '*',
-  }
+    origin: "*",
+  },
 });
 
 app.use(cors());
@@ -35,197 +45,219 @@ app.use(cors());
 readCardList();
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World");
+  res.send("Hello World");
 });
 
 let roomId = 0;
 
 function joinRoom(socket: Socket, player: Player, roomId: string) {
-    let newRoom = getRoomById(roomId);
-    if(newRoom) {
-        // If the game has already started don't let the player in
-        if(newRoom.gameStarted) {
-            return;
-        }
+  let newRoom = getRoomById(roomId);
+  if (newRoom) {
+    // If the game has already started don't let the player in
+    if (newRoom.gameStarted) {
+      return;
     }
+  }
 
-    // Leave the current room
-    if(player.currentRoom) {
-        let room = getRoomById(player.currentRoom);
-        if(room) {
-            room.leave(player);
-            socket.leave(room.id);
-        }
+  // Leave the current room
+  if (player.currentRoom) {
+    let room = getRoomById(player.currentRoom);
+    if (room) {
+      room.leave(player);
+      socket.leave(room.id);
     }
+  }
 
-    // Join the new room
-    if(newRoom) {
-        newRoom.join(player);
+  // Join the new room
+  if (newRoom) {
+    newRoom.join(player);
 
-        let playerObjects = newRoom!.getPlayerObjects()
-            .map(p => p.toClientObject());
-        socket.emit("client:joinedRoom", 
-            newRoom.toClientObject(), playerObjects);
+    let playerObjects = newRoom!
+      .getPlayerObjects()
+      .map((p) => p.toClientObject());
+    socket.emit("client:joinedRoom", newRoom.toClientObject(), playerObjects);
 
-        io.to(newRoom.id).emit("room:playerJoin", player.toClientObject());
-        socket.join(newRoom.id);
-    }
+    io.to(newRoom.id).emit("room:playerJoin", player.toClientObject());
+    socket.join(newRoom.id);
+  }
 }
 
 function leaveRoom(socket: Socket, player: Player, room: Room) {
-    room.leave(player);
-    socket.leave(room.id);
-    socket.emit("client:leaveRoom");
+  room.leave(player);
+  socket.leave(room.id);
+  socket.emit("client:leaveRoom");
 
-    if(room.playerIds.size <= 0) {
-        deleteRoomById(room.id);
-    } else {
-        io.to(room.id).emit("room:playerLeave", player.toClientObject());
+  if (room.playerIds.size <= 0) {
+    deleteRoomById(room.id);
+  } else {
+    io.to(room.id).emit("room:playerLeave", player.toClientObject());
 
-        if(room.owner === player.id) {
-            // Change the owner
-            // TODO(patrik): Move this to room.pickNewOwner()
-            let newOwner = room.playerIds.values().next().value;
-            // TODO(patrik): Check if the player exists?
-            room.owner = newOwner;
-            io.to(room.id).emit("room:changed", room.toClientObject());
-        }
+    if (room.owner === player.id) {
+      // Change the owner
+      // TODO(patrik): Move this to room.pickNewOwner()
+      let newOwner = room.playerIds.values().next().value;
+      // TODO(patrik): Check if the player exists?
+      room.owner = newOwner;
+      io.to(room.id).emit("room:changed", room.toClientObject());
     }
+  }
 }
 
 function playerLogout(socket: Socket) {
-    let player = getPlayerBySocket(socket);
-    if(player) {
-        if(player.currentRoom) {
-            let room = getRoomById(player.currentRoom);
-            if(room) {
-                leaveRoom(socket, player, room);
-            }
-        }
-
-        players.delete(socket.id);
+  let player = getPlayerBySocket(socket);
+  if (player) {
+    if (player.currentRoom) {
+      let room = getRoomById(player.currentRoom);
+      if (room) {
+        leaveRoom(socket, player, room);
+      }
     }
+
+    players.delete(socket.id);
+  }
 }
 
 io.on("connection", (socket: Socket) => {
-    console.log(`Connection ${socket.id}`);
+  console.log(`Connection ${socket.id}`);
 
-    socket.on("client:login", (data, callback) => {
-        registerNewPlayer(new Player(socket.id, data.username));
-        callback(getPlayerBySocket(socket)!.toClientObject());
+  socket.on("client:login", (data, callback) => {
+    registerNewPlayer(new Player(socket.id, data.username));
+    callback(getPlayerBySocket(socket)!.toClientObject());
 
-        socket.on("client:logout", () => {
-            playerLogout(socket);
-        });
-
-        socket.on("rooms:get", (callback) => {
-            let data = [...rooms.values()].map(room => room.toClientObject());
-            callback(data);
-        });
-
-        socket.on("rooms:join", (id) => {
-            let player = getPlayerBySocket(socket);
-            let room = getRoomById(id);
-            if(player && room) {
-                console.log(`${player.username} joins '${room.id}'`)
-
-                joinRoom(socket, player, room.id);
-            }
-        });
-
-        socket.on("rooms:leave", () => {
-            let player = getPlayerBySocket(socket);
-            if(player && player.currentRoom) {
-                let room = getRoomById(player.currentRoom);
-                if(room) {
-                    leaveRoom(socket, player, room);
-                }
-            }
-        });
-
-        socket.on("rooms:create", (name, callback) => {
-            let player = getPlayerBySocket(socket);
-            if(player && !player.currentRoom) {
-                let id = `${roomId}`;
-                let newRoom = new Room(id, name, player.id);
-                registerNewRoom(newRoom);
-                console.log(`Creating new room '${id}: ${name}`);
-
-                joinRoom(socket, player, id);
-
-                callback(getRoomById(id)!.toClientObject());
-
-                roomId++;
-            }
-        });
-
-        socket.on("room:startGame", () => {
-            let player = getPlayerBySocket(socket);
-            if(player && player.currentRoom) {
-                let room = getRoomById(player.currentRoom);
-                if(room) {
-                    if(player.id === room.owner && !room.gameStarted) {
-                        // Start the game
-                        let game = room.startGame();
-                        games.set(room.id, game);
-
-                        io.to(room.id).emit("room:startedGame");
-
-                        game.startGame(io);
-                    }
-                }
-            }
-        });
-
-        socket.on("game:forceNextRound", () => {
-            let player = getPlayerBySocket(socket);
-            if(player && player.currentRoom) {
-                let room = getRoomById(player.currentRoom);
-                if(room) {
-                    if(player.id === room.owner && room.gameStarted) {
-                        let game = games.get(room.id);
-                        if(game) {
-                            game.nextRound(io);
-                        }
-                    }
-                }
-            }
-        });
-
-        socket.on("game:play", (hand_index: number) => {
-            let player = getPlayerBySocket(socket);
-            if(player && player.currentRoom) {
-                let room = getRoomById(player.currentRoom);
-                if(room) {
-                    let game = games.get(room.id);
-                    if(game) {
-                        game.playerPlay(io, player, hand_index);
-                    }
-                }
-            }
-        });
-
-        socket.on("game:judgeSelect", (pairIndex: number) => {
-            let player = getPlayerBySocket(socket);
-            if(player && player.currentRoom) {
-                let room = getRoomById(player.currentRoom);
-                if(room) {
-                    let game = games.get(room.id);
-                    if(game) {
-                        game.judgeSelect(io, player, pairIndex);
-                    }
-                }
-            }
-        });
+    socket.on("client:logout", () => {
+      playerLogout(socket);
     });
 
-    socket.on("disconnect", () => {
-        console.log(`Disconnect ${socket.id}`);
+    socket.on("rooms:get", (callback) => {
+      let data = [...rooms.values()].map((room) => room.toClientObject());
+      callback(data);
+    });
 
-        playerLogout(socket);
-    })
+    socket.on("rooms:join", (id) => {
+      let player = getPlayerBySocket(socket);
+      let room = getRoomById(id);
+      if (player && room) {
+        console.log(`${player.username} joins '${room.id}'`);
+
+        joinRoom(socket, player, room.id);
+      }
+    });
+
+    socket.on("rooms:leave", () => {
+      let player = getPlayerBySocket(socket);
+      if (player && player.currentRoom) {
+        let room = getRoomById(player.currentRoom);
+        if (room) {
+          leaveRoom(socket, player, room);
+        }
+      }
+    });
+
+    socket.on("rooms:create", (name, callback) => {
+      let player = getPlayerBySocket(socket);
+      if (player && !player.currentRoom) {
+        let id = `${roomId}`;
+        let newRoom = new Room(id, name, player.id);
+        registerNewRoom(newRoom);
+        console.log(`Creating new room '${id}: ${name}`);
+
+        joinRoom(socket, player, id);
+
+        callback(getRoomById(id)!.toClientObject());
+
+        roomId++;
+      }
+    });
+
+    socket.on("room:startGame", () => {
+      let player = getPlayerBySocket(socket);
+      if (player && player.currentRoom) {
+        let room = getRoomById(player.currentRoom);
+        if (room) {
+          if (player.id === room.owner && !room.gameStarted) {
+            // Start the game
+            let game = room.startGame();
+            games.set(room.id, game);
+
+            io.to(room.id).emit("room:startedGame");
+
+            game.startGame(io);
+          }
+        }
+      }
+    });
+
+    socket.on("room:getGameSettings", (callback) => {
+      let player = getPlayerBySocket(socket);
+      if (player && player.currentRoom) {
+        let room = getRoomById(player.currentRoom);
+        if (room) {
+          let settings = room.getGameSettings();
+          let packs = getCardPacks();
+          callback(settings, packs);
+        }
+      }
+    });
+
+    socket.on("room:setGameSettings", (newSettings: GameSettings) => {
+      let player = getPlayerBySocket(socket);
+      if (player && player.currentRoom) {
+        let room = getRoomById(player.currentRoom);
+        if (room) {
+          room.setGameSettings(newSettings);
+        }
+      }
+    });
+
+    socket.on("game:forceNextRound", () => {
+      let player = getPlayerBySocket(socket);
+      if (player && player.currentRoom) {
+        let room = getRoomById(player.currentRoom);
+        if (room) {
+          if (player.id === room.owner && room.gameStarted) {
+            let game = games.get(room.id);
+            if (game) {
+              game.nextRound(io);
+            }
+          }
+        }
+      }
+    });
+
+    socket.on("game:play", (hand_index: number) => {
+      let player = getPlayerBySocket(socket);
+      if (player && player.currentRoom) {
+        let room = getRoomById(player.currentRoom);
+        if (room) {
+          let game = games.get(room.id);
+          if (game) {
+            game.playerPlay(io, player, hand_index);
+          }
+        }
+      }
+    });
+
+    socket.on("game:judgeSelect", (pairIndex: number) => {
+      let player = getPlayerBySocket(socket);
+      if (player && player.currentRoom) {
+        let room = getRoomById(player.currentRoom);
+        if (room) {
+          let game = games.get(room.id);
+          if (game) {
+            game.judgeSelect(io, player, pairIndex);
+          }
+        }
+      }
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`Disconnect ${socket.id}`);
+
+    playerLogout(socket);
+  });
 });
 
 server.listen(port, () => {
-    console.log(`Running server on port ${port}`);
+  console.log(`Running server on port ${port}`);
 });
--- server/room.ts
+++ server/room.ts
@@ -4,75 +4,82 @@ import { getAllPlayers, Player } from "./player";
 export const rooms = new Map<string, Room>();
 
 export interface ClientRoom {
-    id: string;
-    name: string;
-    owner: string,
-    playerCount: number,
-    gameStarted: boolean,
+  id: string;
+  name: string;
+  owner: string;
+  playerCount: number;
+  gameStarted: boolean;
 }
 
 export class Room {
-    id: string;
-    name: string;
-    owner: string;
-    playerIds: Set<string>;
-    gameStarted: boolean;
-    gameSettings: GameSettings;
+  id: string;
+  name: string;
+  owner: string;
+  playerIds: Set<string>;
+  gameStarted: boolean;
+  gameSettings: GameSettings;
 
-    constructor(id: string, name: string, owner: string) {
-        this.id = id;
-        this.name = name;
-        this.owner = owner;
-        this.playerIds = new Set();
-        this.gameStarted = false;
-        this.gameSettings = defaultGameSettings;
-    }
+  constructor(id: string, name: string, owner: string) {
+    this.id = id;
+    this.name = name;
+    this.owner = owner;
+    this.playerIds = new Set();
+    this.gameStarted = false;
+    this.gameSettings = defaultGameSettings;
+  }
 
-    join(player: Player) {
-        // TODO(patrik): Do we need to check if the player already has 
-        // joined this room?
-        player.currentRoom = this.id;
-        this.playerIds.add(player.id);
-    }
+  getGameSettings() {
+    return this.gameSettings;
+  }
 
-    leave(player: Player) {
-        // TODO(patrik): Do we need to check if the player already has 
-        // joined this room?
-        // TODO(patrik): We should check currentRoom === this.id
-        player.currentRoom = undefined;
-        this.playerIds.delete(player.id);
-    }
+  setGameSettings(newSettings: GameSettings) {
+    this.gameSettings = newSettings;
+  }
 
-    startGame() {
-        this.gameStarted = true;
-        return new Game(this.id);
-    }
+  join(player: Player) {
+    // TODO(patrik): Do we need to check if the player already has
+    // joined this room?
+    player.currentRoom = this.id;
+    this.playerIds.add(player.id);
+  }
 
-    getPlayerObjects() {
-        return getAllPlayers()
-            .filter(item => this.playerIds.has(item.id));
-    }
+  leave(player: Player) {
+    // TODO(patrik): Do we need to check if the player already has
+    // joined this room?
+    // TODO(patrik): We should check currentRoom === this.id
+    player.currentRoom = undefined;
+    this.playerIds.delete(player.id);
+  }
 
-    toClientObject(): ClientRoom {
-        let playerCount = this.playerIds.size;
-        return {
-            id: this.id,
-            name: this.name,
-            owner: this.owner,
-            playerCount: playerCount,
-            gameStarted: this.gameStarted,
-        };
-    }
+  startGame() {
+    this.gameStarted = true;
+    return new Game(this.id, this.gameSettings);
+  }
+
+  getPlayerObjects() {
+    return getAllPlayers().filter((item) => this.playerIds.has(item.id));
+  }
+
+  toClientObject(): ClientRoom {
+    let playerCount = this.playerIds.size;
+    return {
+      id: this.id,
+      name: this.name,
+      owner: this.owner,
+      playerCount: playerCount,
+      gameStarted: this.gameStarted,
+    };
+  }
 }
 
 export function getRoomById(roomId: string) {
-    return rooms.get(roomId);
+  return rooms.get(roomId);
 }
 
 export function deleteRoomById(roomId: string) {
-    rooms.delete(roomId);
+  rooms.delete(roomId);
 }
 
 export function registerNewRoom(room: Room) {
-    rooms.set(room.id, room);
+  rooms.set(room.id, room);
 }
